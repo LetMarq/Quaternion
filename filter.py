b4d06afcc172342489aa24bb5e3384d5bf53736c
Letícia Marques <leeh.marques157@gmail.com>
2021-05-22 16:38:16 -0300

    Sensitivity change

--- filter.py
+++ filter.py
@@ -24,7 +24,12 @@ def read_array_from_line(line):
     return np.array(list(map(float, line.replace('[', '').replace(']', '').strip().split(','))))
 
 def separate_gyr_acc(data_array):
-    return np.array([data_array[0:3], data_array[3:6]])
+    acc_sens = 16384 / 16.4
+    gyr_sens = 1
+    return [
+        np.array(list(map(lambda x: x / acc_sens, data_array[0:3]))), 
+        np.array(list(map(lambda x: x / gyr_sens, data_array[3:6])))
+        ]
 
 def lines(file1, file2):
     return list(map(lambda line1, line2: 
@@ -38,7 +43,7 @@ def integrator(old_gyr_angle, new_gyr_angle, dt):
     return old_gyr_angle + new_gyr_angle * dt
 
 def low_pass_filter(gyr_angle, acc_coord):
-    return gyr_angle + (0.98 * gyr_angle + 0.02 * acc_coord)
+    return  0.98 * gyr_angle + 0.02 * acc_coord
 
 def complementary_filter(old_gyr_angles, new_gyr_angles, acc_coords):
     dt = 0.02
@@ -57,7 +62,6 @@ def main(filename1, filename2):
     with open(filename1, 'r') as file1, open(filename2, 'r') as file2:
         old_gyr_angles_1 = np.array([0, 0, 0])
         old_gyr_angles_2 = np.array([0, 0, 0])
-
         pPulso = np.array([15,15,-15])
         pCotovelo = np.array([15,0,-15])
         pOmbro = np.array([15,0,0])
@@ -73,12 +77,18 @@ def main(filename1, filename2):
         ax.set_ylim([-25,25])
         ax.set_zlim([-25,25])
          #Seleciona o angulo de visão do plano 3d
+        t = np.arange(0, 10, 10./280.)
+        x = []
+        y = []
+        z = []
+        axis = 1
         
-        for data in lines(file1, file2):
-            [data_1, data_2] = data
-            [new_gyr_angles_1, acc_coords_1] = data_1
-            [new_gyr_angles_2, acc_coords_2] = data_2
-            [old_gyr_angles_1, old_gyr_angles_2, quat_1, quat_2] = double_complementary_filter(old_gyr_angles_1, new_gyr_angles_1, acc_coords_1, old_gyr_angles_2, new_gyr_angles_2, acc_coords_2)
+        for [[acc_coords_1, new_gyr_angles_1], [acc_coords_2, new_gyr_angles_2]] in lines(file1, file2):
+            [old_gyr_angles_1, old_gyr_angles_2, quat_1, quat_2] = double_complementary_filter(
+                old_gyr_angles_1, new_gyr_angles_1, acc_coords_1, old_gyr_angles_2, new_gyr_angles_2, acc_coords_2)
+            x.append(new_gyr_angles_2[axis])
+            y.append(old_gyr_angles_2[axis])
+            z.append(acc_coords_2[axis])
             final_1 = newPosition(pCotovelo, pPulso, quat_1, quat_2)
             ax.scatter(pCotovelo[0],pCotovelo[1],pCotovelo[2],color='red')
             ax.scatter(pOmbro[0],pOmbro[1],pOmbro[2],color='blue')
@@ -86,7 +96,7 @@ def main(filename1, filename2):
             ax.plot([final_1[0],pCotovelo[0]],[final_1[1],pCotovelo[1]],[final_1[2],pCotovelo[2]],color='black')
             ax.plot([pOmbro[0],pCotovelo[0]],[pOmbro[1],pCotovelo[1]],[pOmbro[2],pCotovelo[2]],color='black')
             ax.plot([pOmbro[0],pEsterno[0]],[pOmbro[1],pEsterno[1]],[pOmbro[2],pEsterno[2]],color='black')            
-            plt.pause(0.02)
+            plt.pause(0.035)
             point_1.remove()
             ax.lines[0].remove()
     
@@ -102,7 +112,10 @@ def main(filename1, filename2):
            #client.loop()
            #if running:
            #    client.publish('harpy/coordinates', f'{points}'.encode('utf8'))
+        plt.plot(t, np.array(z), c = 'r')
+        plt.plot(t, np.array(y), c = 'g')
+        plt.plot(t, np.array(x), c = 'b')
         plt.show()
 
 if __name__ == '__main__':
-    main('parado_10s (1).txt', 'vertical_p_horizontal_10s (2).txt')
+    main('parado_10s (1).txt', 'mexendo_2x_10s.txt')
